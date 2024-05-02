2376977de339c72d96e857b811243436dd2f5283
Anthony Castillo <anthony1093ca@gmail.com>
2024-05-02 12:55:16 -0700

    Progressing

--- dataHandler.py
+++ dataHandler.py
@@ -10,6 +10,10 @@ import tensorflow as tf
 import seaborn as sns
 from windowGenerator import WindowGenerator
 from baseline import Baseline
+import IPython
+import IPython.display
+from threading import Thread
+import time
 
 class DataHandler():
     def __init__(self):
@@ -21,6 +25,8 @@ class DataHandler():
         self.__validWavelengthCount = 0
         self.__validWavelengths = []
         self.__AODTotalColumns = []
+        self.__threads = []
+        self.__num_features = 0
 
     def __del__(self):
         pass
@@ -141,7 +147,7 @@ class DataHandler():
         train_df = self.__data[0:int(n*0.7)]
         val_df = self.__data[int(n*0.7):int(n*0.9)]
         test_df = self.__data[int(n*0.9):]
-        num_features = self.__data.shape[1]
+        self.__num_features = self.__data.shape[1]
         train_mean = train_df.mean()
         train_std = train_df.std()
 
@@ -231,13 +237,218 @@ class DataHandler():
         print('Output shape:', linear(wide_window.example[0]).shape)
 
         wide_window.plot(linear)
-
+        # plt.show()
         plt.bar(x = range(len(self.__normalizedData[0].columns)),
-        height=linear.layers[0].kernel[:,0].numpy())
+                height=linear.layers[0].kernel[:,0].numpy())
         axis = plt.gca()
         axis.set_xticks(range(len(self.__normalizedData[0].columns)))
         _ = axis.set_xticklabels(self.__normalizedData[0].columns, rotation=90)
         plt.show()
+
+        dense = tf.keras.Sequential([
+            tf.keras.layers.Dense(units=64, activation='relu'),
+            tf.keras.layers.Dense(units=64, activation='relu'),
+            tf.keras.layers.Dense(units=1)
+        ])
+
+        history = self.compile_and_fit(dense, single_step_window)
+
+        val_performance['Dense'] = dense.evaluate(single_step_window.val, return_dict=True)
+        performance['Dense'] = dense.evaluate(single_step_window.test, verbose=0, return_dict=True)
+
+        CONV_WIDTH = 3
+        conv_window = WindowGenerator(
+            input_width=CONV_WIDTH,
+            label_width=1,
+            shift=1,
+            train_df=self.__normalizedData[0], 
+            val_df=self.__normalizedData[1], 
+            test_df=self.__normalizedData[2],
+            label_columns=['AOD_380nm-Total'])
+
+        conv_window
+        conv_window.plot()
+        plt.suptitle("Given 3 hours of inputs, predict 1 hour into the future.")
+        plt.show()
+
+        multi_step_dense = tf.keras.Sequential([
+            # Shape: (time, features) => (time*features)
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(units=32, activation='relu'),
+            tf.keras.layers.Dense(units=32, activation='relu'),
+            tf.keras.layers.Dense(units=1),
+            # Add back the time dimension.
+            # Shape: (outputs) => (1, outputs)
+            tf.keras.layers.Reshape([1, -1]),
+        ])
+
+        print('Input shape:', conv_window.example[0].shape)
+        print('Output shape:', multi_step_dense(conv_window.example[0]).shape)
+
+        history = self.compile_and_fit(multi_step_dense, conv_window)
+
+        IPython.display.clear_output()
+        val_performance['Multi step dense'] = multi_step_dense.evaluate(conv_window.val, return_dict=True)
+        performance['Multi step dense'] = multi_step_dense.evaluate(conv_window.test, verbose=0, return_dict=True)
+
+        conv_window.plot(multi_step_dense)
+        plt.show()
+        
+        print('Input shape:', wide_window.example[0].shape)
+        try:
+            print('Output shape:', multi_step_dense(wide_window.example[0]).shape)
+        except Exception as e:
+            print(f'\n{type(e).__name__}:{e}')
+
+        conv_model = tf.keras.Sequential([
+            tf.keras.layers.Conv1D(filters=32,
+                                kernel_size=(CONV_WIDTH,),
+                                activation='relu'),
+            tf.keras.layers.Dense(units=32, activation='relu'),
+            tf.keras.layers.Dense(units=1),
+        ])
+
+        print("Conv model on `conv_window`")
+        print('Input shape:', conv_window.example[0].shape)
+        print('Output shape:', conv_model(conv_window.example[0]).shape)
+
+        history = self.compile_and_fit(conv_model, conv_window)
+
+        IPython.display.clear_output()
+        val_performance['Conv'] = conv_model.evaluate(conv_window.val, return_dict=True)
+        performance['Conv'] = conv_model.evaluate(conv_window.test, verbose=0, return_dict=True)
+
+        print("Wide window")
+        print('Input shape:', wide_window.example[0].shape)
+        print('Labels shape:', wide_window.example[1].shape)
+        print('Output shape:', conv_model(wide_window.example[0]).shape)
+
+        LABEL_WIDTH = 24
+        INPUT_WIDTH = LABEL_WIDTH + (CONV_WIDTH - 1)
+        wide_conv_window = WindowGenerator(
+            input_width=INPUT_WIDTH,
+            label_width=LABEL_WIDTH,
+            shift=1,
+            train_df=self.__normalizedData[0], 
+            val_df=self.__normalizedData[1], 
+            test_df=self.__normalizedData[2],
+            label_columns=['AOD_380nm-Total'])
+
+        print(wide_conv_window)
+
+        print("Wide conv window")
+        print('Input shape:', wide_conv_window.example[0].shape)
+        print('Labels shape:', wide_conv_window.example[1].shape)
+        print('Output shape:', conv_model(wide_conv_window.example[0]).shape)
+
+        wide_conv_window.plot(conv_model)
+        plt.show()
+
+        lstm_model = tf.keras.models.Sequential([
+            # Shape [batch, time, features] => [batch, time, lstm_units]
+            tf.keras.layers.LSTM(32, return_sequences=True),
+            # Shape => [batch, time, features]
+            tf.keras.layers.Dense(units=1)
+        ])
+
+        print('Input shape:', wide_window.example[0].shape)
+        print('Output shape:', lstm_model(wide_window.example[0]).shape)
+
+        history = self.compile_and_fit(lstm_model, wide_window)
+
+        IPython.display.clear_output()
+        val_performance['LSTM'] = lstm_model.evaluate(wide_window.val, return_dict=True)
+        performance['LSTM'] = lstm_model.evaluate(wide_window.test, verbose=0, return_dict=True)
+
+        wide_window.plot(lstm_model)
+        plt.show()
+
+        cm = lstm_model.metrics[1]
+        print(cm.metrics)
+
+        print(val_performance)
+
+        x = np.arange(len(performance))
+        width = 0.3
+        metric_name = 'mean_absolute_error'
+        val_mae = [v[metric_name] for v in val_performance.values()]
+        test_mae = [v[metric_name] for v in performance.values()]
+
+        plt.ylabel('mean_absolute_error [T (degC), normalized]')
+        plt.bar(x - 0.17, val_mae, width, label='Validation')
+        plt.bar(x + 0.17, test_mae, width, label='Test')
+        plt.xticks(ticks=x, labels=performance.keys(),
+                rotation=45)
+        _ = plt.legend()
+        plt.show()
+
+        for name, value in performance.items():
+            print(f'{name:12s}: {value[metric_name]:0.4f}')
+
+        single_step_window = WindowGenerator(
+            # `WindowGenerator` returns all features as labels if you
+            # don't set the `label_columns` argument.
+            input_width=1, label_width=1, shift=1,
+            train_df=self.__normalizedData[0], 
+            val_df=self.__normalizedData[1], 
+            test_df=self.__normalizedData[2],
+            )
+
+        wide_window = WindowGenerator(
+            input_width=24, label_width=24, shift=1,
+            train_df=self.__normalizedData[0], 
+            val_df=self.__normalizedData[1], 
+            test_df=self.__normalizedData[2],
+            )
+
+        for example_inputs, example_labels in wide_window.train.take(1):
+            print(f'Inputs shape (batch, time, features): {example_inputs.shape}')
+            print(f'Labels shape (batch, time, features): {example_labels.shape}')
+
+        baseline = Baseline()
+        baseline.compile(loss=tf.keras.losses.MeanSquaredError(),
+                        metrics=[tf.keras.metrics.MeanAbsoluteError()])
+        
+        val_performance = {}
+        performance = {}
+        val_performance['Baseline'] = baseline.evaluate(wide_window.val, return_dict=True)
+        performance['Baseline'] = baseline.evaluate(wide_window.test, verbose=0, return_dict=True)
+
+        dense = tf.keras.Sequential([
+            tf.keras.layers.Dense(units=64, activation='relu'),
+            tf.keras.layers.Dense(units=64, activation='relu'),
+            tf.keras.layers.Dense(units=self.__num_features)
+        ])
+
+        history = self.compile_and_fit(dense, single_step_window)
+
+        IPython.display.clear_output()
+        val_performance['Dense'] = dense.evaluate(single_step_window.val, return_dict=True)
+        performance['Dense'] = dense.evaluate(single_step_window.test, verbose=0, return_dict=True)
+
+        wide_window = WindowGenerator(
+            input_width=24, label_width=24, shift=1,
+            train_df=self.__normalizedData[0], 
+            val_df=self.__normalizedData[1], 
+            test_df=self.__normalizedData[2],
+            )
+
+        lstm_model = tf.keras.models.Sequential([
+            # Shape [batch, time, features] => [batch, time, lstm_units]
+            tf.keras.layers.LSTM(32, return_sequences=True),
+            # Shape => [batch, time, features]
+            tf.keras.layers.Dense(units=self.__num_features)
+        ])
+
+        history = self.compile_and_fit(lstm_model, wide_window)
+
+        IPython.display.clear_output()
+        val_performance['LSTM'] = lstm_model.evaluate( wide_window.val, return_dict=True)
+        performance['LSTM'] = lstm_model.evaluate( wide_window.test, verbose=0, return_dict=True)
+
+        # print()
+
+        
     # end window
 
     def compile_and_fit(self, model, window, patience=2, MAX_EPOCHS=20):
